e85c0b06a0e435640b0bd06dc152c14c8c75f7a5
Mystique <zorinatrader@gmail.com>
2023-03-08 19:16:57 +0300

    source array

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите данные через пробел: ");
+string item = Console.ReadLine();
+string[] sourceArray = item.Split(' ');
+Console.WriteLine("Исходный массив данных: [{0}]", string.Join(", ", sourceArray));
+
+string[] targetArray = new string[sourceArray.Length];
+int targetIndex = 0;
+
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    if (sourceArray[i].Length <= 3)
+    {
+        targetArray[targetIndex] = sourceArray[i];
+        targetIndex++;
+    }
+}
+
+Array.Resize(ref targetArray, targetIndex);
+
+Console.WriteLine("Итоговый массив данных: [{0}]", string.Join(", ", targetArray));
